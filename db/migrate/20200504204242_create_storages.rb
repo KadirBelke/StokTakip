6451c5379ea29e5918b0a868ca32bf1ba2d45f02
Kadir Belke <belkekadir@gmail.com>
2020-05-05 02:35:30 +0300

    menü tasarlandı

---
+++ db/migrate/20200504204242_create_storages.rb
@@ -0,0 +1,10 @@
+class CreateStorages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :storages do |t|
+      t.integer :no
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
