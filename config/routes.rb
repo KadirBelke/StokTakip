ab33af186214166bc66bf3485d26bb23fa38ea50
Kadir Belke <belkekadir@gmail.com>
2020-05-21 21:26:47 +0300

    kullanıcı işlemleri başladı

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'storage#index'
   devise_for :users
 
+  resources :users, only: %i[index]
   resources :storage,  only: %i[index create update destroy]
   resources :materials,  only: %i[create update destroy]
   get 'storage/:id/material', to: "materials#index", as: 'materials_index'
