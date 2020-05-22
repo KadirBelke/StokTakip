1bcfb89e04b04360905549a21d55da824705f84a
Kadir Belke <belkekadir@gmail.com>
2020-05-22 16:40:59 +0300

    kullanıcı onaylama işlmeleri yapılıoyr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   layout :layout
   before_action :authenticate_user!
   before_action :configure_sign_up_params, if: :devise_controller?
-
+  before_action :user_waiting?, if: :user_signed_in?
   private
   def layout
     devise_controller? ? "devise" : "application"
@@ -12,4 +12,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname, :title])
   end
 
+  def user_waiting?
+    if current_user.waiting?
+      sign_out
+      flash[:error] = "Kullanıcı onayı bekleniyor"
+      redirect_to new_user_session_path
+    end
+  end
 end
