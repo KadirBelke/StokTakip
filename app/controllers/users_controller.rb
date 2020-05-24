0ed882c34f1eddba878c0e43e391d280ea472633
Kadir Belke <belkekadir@gmail.com>
2020-05-25 01:30:00 +0300

    parola güncelleme aktifleştirildi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,41 @@ class UsersController < ApplicationController
   def index
     @users = User.all.sort_by{|u| u[:created_at]}
   end
+
+  def update
+    user = User.find(params[:id])
+    if user.approved!
+      flash[:success] = "Güncellendi"
+      redirect_to users_path
+    end
+  end
+
+  def update_password
+    user = User.find(current_user.id)
+    if user.valid_password?(params[:user][:current_password])
+      if user.update password_params
+        flash[:success] = "Parola Güncellendi"
+        redirect_back fallback_location: root_path
+      else
+        flash[:error] = "Parola Güncellenemedi "
+        redirect_back fallback_location: root_path
+      end
+    else
+      flash[:error] = "Eski Parola Hatalı"
+      redirect_back fallback_location: root_path
+    end
+
+  end
+
+  private
+  def admin?
+    unless current_user.admin?
+      root_path
+    end
+  end
+
+  def password_params
+      params.require(:user).permit(:password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'storage#index'
   devise_for :users
 
-  resources :users, only: %i[index]
+  resources :users, only: %i[index update]
+  patch 'password_update/:id', to: "users#update_password", as: "password_update"
   resources :storage,  only: %i[index create update destroy]
   resources :materials,  only: %i[create update destroy]
   get 'storage/:id/material', to: "materials#index", as: 'materials_index'
