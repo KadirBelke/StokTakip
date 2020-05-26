71882dd06a7379ae7c6bdc565a2b7cf72d35bd97
Kadir Belke <belkekadir@gmail.com>
2020-05-27 01:22:34 +0300

    raporlama bitti

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,8 @@ class ReportsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
+    @material_based = Operation.where(category: :minus).group(:material).sum(:piece).sort_by{|k,v| v}.reverse
+    @user_based = Operation.where(category: :minus).group(:user).sum(:piece).sort_by{|k,v| v}.reverse
   end
+
 end
